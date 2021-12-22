c329b4701f226654d38f10a929989056ddbce409
tsobczak11 <tsobczak011@gmail.com>
2021-12-21 18:03:22 -0600

    Updated home page

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Welcome to Tommy Sobczak's Personal Website
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming Soon...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
